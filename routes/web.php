0ee217364541fd5d0bcd0701b483023acb6772b2
Woody Chang <kazettique@gmail.com>
2023-12-02 18:15:21 +0800

    dynamic routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return '<h4>contact page</h4>';
+});
+
+// query strings
+// Route::get('/store', function () {
+//     $category = request('category');
+
+//     if (isset($category)) {
+//         return 'your category: ' . $category;
+//     }
+
+//     return 'view all categories';
+// });
+
+// dynamic paths
+Route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
+    $category = request('category');
+
+    if (isset($category)) {
+        if (isset($item)) {
+            return "you are viewing the store for {$category} for {$item}";
+        }
+
+        return "you are viewing the store for {$category}";
+    }
+
+    return 'you are viewing all instruments';
+});
